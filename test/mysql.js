ee120dc81cb0f5c349a30b7249b17ee53d62ab3d
Levon Kirakosyan <noosx.e@gmail.com>
2014-05-31 12:59:07 +0400

    [mysql]: #delete() method

--- test/mysql.js
+++ test/mysql.js
@@ -153,4 +153,17 @@ describe('query.mysql', function() {
 
 	});
 
+	describe('#delete()', function() {
+
+		it('should set query type to "delete"', function() {
+			expect(query('User').delete()._type).to.be.equal('delete');
+		});
+
+		it('should return chaining object', function() {
+			var q = query('User');
+			expect(q.delete()).to.be.equal(q);
+		});
+
+	});
+
 });
