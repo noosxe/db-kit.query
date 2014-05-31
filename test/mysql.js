9e588d1c552789465782f0bd7823310c362865ef
Levon Kirakosyan <noosx.e@gmail.com>
2014-05-31 12:51:48 +0400

    #update() method

--- test/mysql.js
+++ test/mysql.js
@@ -134,4 +134,23 @@ describe('query.mysql', function() {
 
 	});
 
+	describe('#update()', function() {
+
+		it('should set update values ', function() {
+			expect(query('User').update({ email: 'example@example.com' }).updateValues).to.be.deep.equal({
+				email: 'example@example.com'
+			});
+		});
+
+		it('should set query type to "update"', function() {
+			expect(query('User').update({ email: 'example@example.com' })._type).to.be.equal('update');
+		});
+
+		it('should return chaining object', function() {
+			var q = query('User');
+			expect(q.update({ email: 'example@example.com' })).to.be.equal(q);
+		});
+
+	});
+
 });
