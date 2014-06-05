731cb5c19935fce0155f958da6d9c6297a459b14
Levon Kirakosyan <noosx.e@gmail.com>
2014-06-05 18:41:48 +0400

    [api]: made mysql query builder available from outside

---
+++ test/api.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var expect = require('chai').expect;
+var query = require('../index.js');
+
+describe('query', function() {
+
+	it('should have "mysql" as property', function() {
+		expect(query).to.have.ownProperty('mysql');
+	});
+
+	describe('query.mysql', function() {
+
+		it('should be function', function() {
+			expect(typeof query.mysql).to.be.equal('function');
+		});
+
+	});
+
+})
